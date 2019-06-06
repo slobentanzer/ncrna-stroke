1f3fbd96c6e5778298e852bda703d5a9e904704d
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2019-06-06 18:39:50 +0200

    added tiedt intersect

--- clusters/update_html_css.sh
+++ clusters/update_html_css.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 find . -name 'index.html' -type f -exec sed -i "s|<div id=\"mainpanel\">|  <div id=\"developercontainer2\">\n    <a href=\"http://rndm4000.github.io/ncrna-stroke/\" title=\"BACK TO MAIN\"><p style=\"font-size:26px; margin-top:24px; margin-bottom: 24px\">BACK TO MAIN PAGE</p></a>\n  </div>\n<div  id=\"mainpanel\">|" {} +
+find . -name 'index.html' -type f -exec sed -i "s|<title>OII Network Visualisation Example</title>|<title>Non-coding RNA in stroke</title>|" {} +
+
 find . -mindepth 2 -name 'style.css' -type f -exec cp -f style.css -T {} \;
